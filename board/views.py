5bf04a767b0c2f35bc37a607748827fb4ab06204
Gerardo Moxca Ruiz <gerardo.mx.rz@gmail.com>
2019-01-16 11:20:40 -0600

    added some comment

--- board/views.py
+++ board/views.py
@@ -10,6 +10,7 @@ from . import serializers, models as board_models
 from accounts import models as account_models
 
 
+# Custom permission (not used yet)
 class AnonCreateAndUpdateOwnerOnly(BasePermission):
     """
     Custom permission:
@@ -25,9 +26,14 @@ class AnonCreateAndUpdateOwnerOnly(BasePermission):
         return view.action in ['retrieve', 'update', 'partial_update'] and obj.user.id == request.user.id or request.user.is_staff
 
 
+# Manager (user) view set
 class ManagerViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ManagerSerializer
 
+    """
+        Gotta check if action is create or action to allow any
+        Otherwise, only if authenticated
+    """
     def get_permissions(self):
 
         if self.action == 'create' or self.action == 'authenticate':
@@ -38,9 +44,17 @@ class ManagerViewSet(viewsets.ModelViewSet):
 
         return [permission() for permission in permission_classes]
 
+    """
+        As we're working with managers (users), we only want those with rol_id=1
+        which are managers
+    """
     def get_queryset(self):
         return account_models.User.objects.filter(rol_id=1)
 
+    """
+        Must hash password with the data from the POST request
+        Check if password is defined in the data, if so hash it
+    """
     def create(self, request, *args, **kwargs):
         if 'password' in request.data:
             request.data['password'] = make_password(request.data['password'])
@@ -50,9 +64,16 @@ class ManagerViewSet(viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
+    """
+        Save/Create user with rol 1
+    """
     def perform_create(self, serializer):
         serializer.save(rol_id=1)
 
+    """
+        Custom action to authenticate user
+        If authenticated return token
+    """
     @action(methods=['POST'], detail=False)
     def authenticate(self, request):
         data = request.data
