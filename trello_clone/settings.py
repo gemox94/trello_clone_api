7cb6cfbf9115c97ce9734b2b39e346d962ca5143
Gerardo Moxca Ruiz <gerardo.mx.rz@gmail.com>
2019-01-16 22:20:08 -0600

    Added cors support

--- trello_clone/settings.py
+++ trello_clone/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'accounts',
     'board',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -149,3 +151,5 @@ REST_FRAMEWORK = {
     #     'user': '10/minute'
     # }
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
